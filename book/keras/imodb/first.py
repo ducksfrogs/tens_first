e765e59b0498d42adb53e1d4145dfe7c33ffe84d
ma <ma_yamaki@yahoo.com>
2021-01-01 20:20:26 +0900

    dd

--- book/keras/imodb/first.py
+++ book/keras/imodb/first.py
@@ -75,11 +75,23 @@ plt.legend()
 
 
 
-acc = history_dict['acc']
-val_acc = history_dict['val_acc']
+acc = history_dict['binary_accuracy']
+val_acc = history_dict['val_binary_accuracy']
 plt.plot(epochs, acc, 'bo', label='Training acc')
 plt.plot(epochs, val_acc, 'b', label='Validation acc')
 plt.title('Training and Validation accuracy')
 plt.xlabel('Epochs')
 plt.ylabel('Accuracy')
 plt.legend()
+
+model = models.Sequential()
+model.add(layers.Dense(16, activation='relu', input_shape=(10000, )))
+model.add(layers.Dense(16, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.compile(optimizer='rmsprop',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(X_train, y_train, epochs=4, batch_size=512)
+results = model.evaluate(X_test, y_test)
