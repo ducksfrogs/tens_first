8d80fec9e5ffa207c53e12dc8a85b9ed03e3a3f9
ma <ma_yamaki@yahoo.com>
2021-01-07 19:58:22 +0900

    dd

--- sound/code/sec.py
+++ sound/code/sec.py
@@ -22,3 +22,18 @@ commands = np.array(tf.io.gfile.listdir(str(data_dir)))
 commands = commands[commands != 'README.md']
 
 filenames = tf.io.gfile.glob(str(data_dir)+ '/*/*')
+filenames = tf.random.shuffle(filenames)
+num_samples = len(filenames)
+print("Number of total examples:", num_samples)
+
+train_files = filenames[:6400]
+val_files = filenames[6400: 6400 + 800]
+test_files = filenames[-800:]
+
+def decode_audio(audio_binary):
+    audio, _ = tf.audio.decode_wav(audio_binary)
+    return tf.squeeze(audio, axis=1)
+
+def get_label(file_path):
+    parts = tf.string.split(file_path, os.path.sep)
+    return parts[-2]
