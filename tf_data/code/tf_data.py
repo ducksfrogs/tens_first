11582de6366bb480c444c84ee04a9edfa85c1230
ducksfrogs <ma_yamaki@yahoo.com>
2021-01-13 20:04:09 +0900

    dd

--- tf_data/code/tf_data.py
+++ tf_data/code/tf_data.py
@@ -38,3 +38,72 @@ for n in range(3):
     display.display(display.Image(image_path))
     print(caption_image(image_path))
     print()
+
+label_names = sorted(item.name for item in data_root.glob('*/') if item.is_dir())
+
+label_to_index = dict((name, index) for index, name in enumerate(label_names))
+
+all_image_labels = [label_to_index[pathlib.Path(path).parent.name] for path in all_image_paths]
+
+print("First 10 labels indices: ", all_image_labels[:10])
+
+img_path = all_image_paths[0]
+img_raw = tf.io.read_file(img_path)
+print(repr(img_raw)[:100]+'...')
+
+img_tensor = tf.image.decode_image(img_raw)
+
+print(int(img_tensor).shape)
+print(img_tensor.dtype)
+
+img_final = tf.image.resize(img_tensor,[192,192])
+img_final = img_final/255.0
+print(img_final.shape)
+print(img_final.numpy().min())
+print(img_final.numpy().max())
+
+
+def preprocess_image(image):
+    image = tf.image.decode_jpeg(image, channels=3)
+    image = tf.image.resize(image, [192,192])
+    image /= 255.0
+    return image
+
+
+
+def load_and_preprocess_image(path):
+    image = tf.io.read_file(path)
+    return preprocess_image(image)
+
+
+import matplotlib.pyplot as plt
+
+image_path = all_image_paths[0]
+label = all_image_labels[0]
+
+plt.imshow(load_and_preprocess_image(image_path))
+plt.grid(False)
+plt.xlabel(caption_image(image_path))
+plt.title(label_names[label].title())
+
+path_ds = tf.data.Dataset.from_tensor_slices(all_image_paths)
+
+print(path_ds)
+
+image_ds = path_ds.map(load_and_preprocess_image, num_parallel_calls=AUTOTUNE)
+
+plt.figure(figsize=(8,8))
+for n, image in enumerate(image_ds.take(4)):
+    plt.subplot(2,2,n+1)
+    plt.imshow(image)
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+    plt.xlabel(caption_image(all_image_paths[n]))
+    plt.show()
+
+
+label_ds = tf.data.Dataset.from_tensor_slices(tf.cast(all_image_labels, tf.int64))
+
+for label in label_ds.take(10):
+    print(label_names[label.numpy()])
