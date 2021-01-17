dce3e4d7b257514803a4b5f7cbe11057edb6f0e1
ma <ma_yamaki@yahoo.com>
2021-01-17 14:51:45 +0900

    d

--- book/keras/chap3/code/over_fit.py
+++ book/keras/chap3/code/over_fit.py
@@ -16,6 +16,9 @@ def vectorize_sequences(sequences, dimension=10000):
 X_train = vectorize_sequences(train_data)
 X_test = vectorize_sequences(test_data)
 
+y_train = np.asarray(train_labels).astype('float32')
+y_test = np.asarray(test_labels).astype('float32')
+
 from keras import models
 from keras import layers
 from keras import optimizers
@@ -28,7 +31,7 @@ model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
 model.add(layers.Dense(16, activation='relu'))
 model.add(layers.Dense(1, activation='sigmoid'))
 
-model.compile(optimizer=optimizers.rmsprop(lr=0.001),
+model.compile(optimizer=optimizers.RMSprop(lr=0.001),
             loss=losses.binary_crossentropy,metrics=[metrics.binary_accuracy])
 
 x_val = X_train[:10000]
@@ -36,3 +39,12 @@ partial_X_train = X_train[10000:]
 
 y_val = y_train[:10000]
 partial_y_train = y_train[10000:]
+
+model.compile(optimizer='rmsprop', loss='binary_crossentropy',
+                metrics=['acc'])
+
+
+history = model.fit(partial_X_train,
+                    partial_y_train, epochs=20,
+                    batch_size=512,
+                    validation_data=(x_val, y_val))
