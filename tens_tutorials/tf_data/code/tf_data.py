c3bc215cd921567fa624658dd21b005f6795344f
ma <ma_yamaki@yahoo.com>
2021-01-26 20:47:50 +0900

    ddada

---
+++ tens_tutorials/tf_data/code/tf_data.py
@@ -0,0 +1,23 @@
+import tensorflow as tf
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+
+import pathlib
+
+data_root_orig = tf.keras.utils.get_file(origin='https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz', fname='flower_photos', untar=True)
+
+data_root = pathlib.Path(data_root_orig)
+print(data_root)
+
+for item in data_root.iterdir():
+    print(item)
+
+import random
+
+all_image_paths = list(data_root.glob('*/*'))
+all_image_paths = [str(path) for path in all_image_paths]
+random.shuffle(all_image_paths)
+
+image_count = len(all_image_paths)
+image_count
+
+all_image_paths[:10]
