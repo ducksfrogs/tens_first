9857a84e47b41cae079fb7259a47d36488e74884
ducksfrogs <ma_yamaki@yahoo.com>
2021-02-02 15:05:04 +0900

    d

--- misc/tf_data_image/code/image.py
+++ misc/tf_data_image/code/image.py
@@ -18,3 +18,81 @@ all_image_paths = list(data_root.glob('*/*'))
 all_image_paths = [str(path) for path in all_image_paths]
 random.shuffle(all_image_paths)
 image_count = len(all_image_paths)
+
+
+import os
+
+attributions = (data_root/"LICENSE.txt").open(encoding='utf-8').readlines()[4:]
+attributions = [line.split(' CC-BY') for line in attributions]
+attributions = dict(attributions)
+
+import IPython.Display as display
+
+def caption_image(image_path):
+    image_rel = pathlib.Path(image_path).relative_to(data_root)
+    return "Image (CC BY 2.0) " + '-'.join(attributions[str(image_rel)].split(' - ' )[:-1])
+
+for n in range(3):
+    image_path = random.choice(all_image_paths)
+    display.display(display.Image(image_path))
+    print(caption_image(image_path))
+    print()
+
+label_names = sorted(item.name for item in data_root.glob('*/') if item.is_dir())
+
+label_to_index = dict((name, index) for index, name in enumerate(label_names))
+
+all_image_labels = [label_to_index[pathlib.Path(path).parent.name] for path in all_image_paths]
+
+
+img_path = all_image_paths[0]
+
+img_raw = tf.io.read_file(image_path)
+print(repr(imt_raw)[:100]+'...')
+
+img_tensor = tf.image.decode_image(img_raw)
+
+print(img_tensor.shape)
+print(img_tensor.dtype)
+
+img_final = tf.image.resize(img_tensor, [192,192])
+img_final = img_final/255.0
+print(img_final.shape)
+print(img_final.numpy().min())
+print(img_final.numpy().max())
+
+def preprocess_image(image):
+    image = tf.image.decode_jpeg(image, channels=3)
+    image = tf.image.resize(image, [192,192])
+    image /= 255.0
+
+    return image
+
+
+def load_and_preprocess_image(path):
+    image = tf.io.read_file(path)
+    return preprocess_image(image)
+
+import matplotlib.pyplot as plt
+
+image_path = all_image_paths[0]
+label = all_image_labels[0]
+
+plt.imshow(load_and_preprocess_image(image_path))
+plt.grid(False)
+plt.xlabel(caption_image(image_path))
+plt.title(label_names[label].title())
+
+path_ds = tf.data.Dataset.from_tensor_slices(all_image_paths)
+
+image_ds = path_ds.map(load_and_preprocess_image, num_parallel_calls=AUTOTUNE)
+
+plt.figure(figsize=(8,8))
+for n, image in enumerate(image_ds.take(4)):
+    plt.subplot(2,2,n+1)
+    plt.imshow(image)
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+    plt.xlabel(caption_image(all_image_paths[n]))
+    plt.show()
