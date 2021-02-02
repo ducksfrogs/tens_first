1914e3558be6a80ea19391638a380f969f1570c9
ducksfrogs <ma_yamaki@yahoo.com>
2021-02-02 17:06:50 +0900

    ddd

--- tens_tutorials/base/code/base_image.py
+++ tens_tutorials/base/code/base_image.py
@@ -15,3 +15,33 @@ class_names = ['T-shirt/top', 'Trouser','Pullover', 'Dress','Coat',
 
 train_images = train_images / 255.0
 test_images = test_images / 255.0
+
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[train_labels[i]])
+
+
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=[28,28]),
+    keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dense(10, activation='softmax')
+])
+
+model.compile(optimizer='adam',
+            loss='sparse_categorical_crossentropy',metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=5)
+
+test_loss, test_acc= model.evaluate(test_images, train_labels, verbose=2)
+
+print('\n Test accuracy: ', test_acc)
+
+pred = model.predict(test_images)
+
+np.argm
