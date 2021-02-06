51e45e6d88f5b282674c02b0c7f3e6eec0fc7d68
ducksfrogs <ma_yamaki@yahoo.com>
2021-02-06 10:14:03 +0900

    dd

--- misc/tf_init/code/hub.py
+++ misc/tf_init/code/hub.py
@@ -38,3 +38,7 @@ history = model.fit(train_data.shuffle(10000).batch(512),
                     epochs=10,
                     validation_data=validation_data.batch(512),
                     verbose=1)
+results = model.evaluate(test_data.batch(512), verbose=2)
+
+for name, value in zip(model.metrics_names, results):
+    print("%s: %.3f" % (name, value))
